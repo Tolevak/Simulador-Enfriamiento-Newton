4dfd8db24d53c8cfa71b00bf5c93726030a4aedd
Tolevak <145602105+Tolevak@users.noreply.github.com>
2025-12-04 12:14:49 -0600

    feat: agregado menu interactivo para pedir datos al usuario
    se implementaron los inputs y la llamada a la función de calculo

--- simulador.py
+++ simulador.py
@@ -43,13 +43,25 @@ def graficar_datos(tiempos, temperaturas):
     pass
 
 def main():
-    # 1. Solicitar datos al usuario
-    print("--- Simulador de Enfriamiento de Newton ---")
-    
-    # 2. Ejecutar simulación
-    
-    # 3. Mostrar resultados
-    print("Simulación finalizada.")
+# 1. Solicitar datos al usuario
+print("--- Simulador de Enfriamiento de Newton ---")
+try:
+   t_ini = float(input("Ingresa Temperatura Inicial del objeto (°C): "))
+   t_amb = float(input("Ingresa Temperatura Ambiente (°C): "))
+   k = float(input("Ingresa constante k: "))
+   total = int(input("Tiempo total a simular (segundos): "))
+      
+# 2. Ejecutar simulación
+print("\nCalculando..")
+eje_x eje_y = calcular_enfriamiento(t_ini, t_amb, k, total)
+
+# 3. Mostrar resultados
+print(f"Simulación finalizada.")
+print(f"Temperatura final del objeto: {eje_y[-1]:.2f} °C after {total} s")
+# pendiente graficar los datos de eje x y
+
+except ValueError:
+print("Error: Por favor ingresa solo números válidos.")
 
 if __name__ == "__main__":
     main()
